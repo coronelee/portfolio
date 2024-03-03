c70edae12f352573d3ad357955ed66f8c42fdf55
coronelee <79922854+coronelee@users.noreply.github.com>
2024-03-03 11:37:16 +0300

    add

--- src/components/ContactsComponent.vue
+++ src/components/ContactsComponent.vue
@@ -1,6 +1,6 @@
 <script setup>
 import axios from 'axios'
-import { onMounted, ref } from 'vue'
+import { onMounted } from 'vue'
 // const clientH = ref(0)
 onMounted(() => {
   //   clientH.value = window.innerHeight
